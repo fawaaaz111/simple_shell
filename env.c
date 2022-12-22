4cd603e44d8aad0baf21a1a3588ffe6ddc2a223c
Eman-A44 <imanalnoor2019@gmail.com>
2022-12-22 06:11:47 -0800

    env

---
+++ env.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *   * __env - displays environment
+ *     * @info: arguments passed
+ *       * Return: int
+*/
+int env(info_t *info)
+{
+	env_t *var;
+	info->status = EXIT_SUCCESS;
+	for (var = info->env; var; var = var->next)
+	{
+		if (var->key)
+			write(STDOUT_FILENO, var->key, _strlen(var->key));
+		write(STDOUT_FILENO, "=", 1);
+		if (var->val)
+			write(STDOUT_FILENO, var->val, _strlen(var->val));
+		write(STDOUT_FILENO, "\n", 1);
+	}
+	return (info->status);
+}
